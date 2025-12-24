357e9166b1111689fd2868e6a1959c74335ce22a
ertosns <mohab-metwally@riseup.net>
2025-12-24 11:32:20 +0200

    Fix static file path to serve under /powerlifting/static

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/powerlifting/static')
 app.secret_key = b'\xf6\x91\nKp\x13\xd4\xf3$!\xa2\x00\xf6\xd4\xa6\x0cB\x89B>\x1b\xf8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///powerlifting_b7.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
